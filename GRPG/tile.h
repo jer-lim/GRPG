e4c80f24934117cc08728f05de26530807968795
Jeremy Lim <git@jerl.im>
2015-01-25 22:21:23 +0800

    Can draw world map sort of

--- GRPG/tile.h
+++ GRPG/tile.h
@@ -10,13 +10,6 @@ namespace tileNS
     const int HEIGHT = 32;                  // image height
     const int X = GAME_WIDTH/2 - WIDTH/2;   // location on screen
     const int Y = GAME_HEIGHT/2 - HEIGHT/2;
-    const float ROTATION_RATE = 0.0f; // radians per second
-    const float SPEED = 0;                // 100 pixels per second
-    const float MASS = 0.0f;              // mass
-    const int   TEXTURE_COLS = 1;           // texture has 8 columns
-    const int   START_FRAME = 0;      // ship1 starts at frame 0
-    const int   END_FRAME = 0;        // ship1 animation frames 0,1,2,3
-    const float ANIMATION_DELAY = 0.0f;    // time between frames
 }
 
 // inherits from Entity class
