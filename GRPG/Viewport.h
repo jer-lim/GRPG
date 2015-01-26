a62ee30ddce0f75c7b63e415b4be2bf215a2b19d
Jeremy Lim <git@jerl.im>
2015-01-26 20:59:10 +0800

    FUCK CHARLES KELLY

--- GRPG/Viewport.h
+++ GRPG/Viewport.h
@@ -1,6 +1,8 @@
 #ifndef VIEWPORT_H
 #define VIEWPORT_H
 
+#include "game.h"
+
 struct Coordinates {
 	float x;
 	float y;
--- GRPG/game.h
+++ GRPG/game.h
@@ -1,7 +1,5 @@
-// Programming 2D Games
-// Copyright (c) 2011 by: 
+// FUCK
 // Charles Kelly
-// Chapter 6 game.h v1.0
 
 #ifndef _GAME_H                 // Prevent multiple definitions if this 
 #define _GAME_H                 // file is included in more than one place
