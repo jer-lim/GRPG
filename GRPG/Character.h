5e4a010012a6fe1f1f6e229610ac88dcdb0959fe
Jeremy Choo <ChooJeremy@Ymail.com>
2015-01-26 15:36:06 +0800

    Character.h now fits class diagram

--- GRPG/Character.h
+++ GRPG/Character.h
@@ -2,43 +2,21 @@
 #define _CHARACTER_H				// file is included in more than one place
 
 #include "constants.h"
-#include <string>
 
 class Character
 {
 private:
-	//Not up to date with class diagram
-	string imgFileName,
-		name,
-		description;
-	int maxHealth;
-	float movementSpeed,
-		attackCooldown,
-		base_dmgReduction;
-	DWORD player_actions = 0x000;
-	/*
-		0000
-		ABCD, where:
-		A: Attackable
-		??
-	*/
+	char* imgFileName;
+	float movementSpeed;
+	float attackCooldown;
+
 public:
 	Character(){}
-	Character(string i, string n, string d, int hp, float mov, float atk, float red){
+	Character(char* i, float mov, float atk)
+	{
 		imgFileName = i;
-		name = n;
-		description = d;
-		maxHealth = hp;
 		movementSpeed = mov;
 		attackCooldown = atk;
-		base_dmgReduction = red;
-	}
-	bool attack(Character otherChar){
-	}
-	bool takeDamage(Character otherChar){
-	}
-	void destroy(){
-
 	}
 };
 
