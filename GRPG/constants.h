0fa062c63f0916013e802882dac682b89f098e5d
Jeremy Lim <git@jerl.im>
2015-01-26 18:24:35 +0800

    Removed useless constants, set frame rate to 60fps

--- GRPG/constants.h
+++ GRPG/constants.h
@@ -42,17 +42,11 @@ const UINT GAME_HEIGHT = 600;               // height of game in pixels
 
 // game
 const double PI = 3.14159265;
-const float FRAME_RATE = 200.0f;                // the target frame rate (frames/sec)
+const float FRAME_RATE = 60.0f;                // the target frame rate (frames/sec)
 const float MIN_FRAME_RATE = 10.0f;             // the minimum frame rate
 const float MIN_FRAME_TIME = 1.0f/FRAME_RATE;   // minimum desired time for 1 frame
 const float MAX_FRAME_TIME = 1.0f/MIN_FRAME_RATE; // maximum time used in calculations
 const float GRAVITY = 6.67428e-11f;             // gravitational constant
-const float MASS_PLANET = 1.0e14f;
-const float MASS_SHIP = 5.0f;
-const UCHAR SHIP_RIGHT_KEY = VK_RIGHT;
-const UCHAR SHIP_LEFT_KEY = VK_LEFT;
-const UCHAR SHIP_UP_KEY = VK_UP;
-const UCHAR SHIP_DOWN_KEY = VK_DOWN;
 
 // graphic images
 const char TEXTURES_IMAGE[] = "assets/npcs/sprites/player.png";  // game textures
