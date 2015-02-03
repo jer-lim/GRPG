3614fe64c42d5b91e25cf137b04e11959b3061ed
Crazoter <crazoter@gmail.com>
2015-02-03 23:18:36 +0800

    fuckthishit

--- GRPG/PickupBehavior.cpp
+++ GRPG/PickupBehavior.cpp
@@ -7,7 +7,8 @@ void PickupBehavior::action(){
 	if (entity->collidesWith(*player, collisionVector))
 	{//if it is range with player
 		player->getInventory()->addEntityInventoryItem(entity, (Grpg*)gamePtr);//add entity into inventory
-		delete this;//THIS MUST BE CALLED IF YOU INTEND TO REMOVE THE BEHAVIOR
+		//delete this;//THIS MUST BE CALLED IF YOU INTEND TO REMOVE THE BEHAVIOR
+		//deleted in destructor to account for merge condition
 	}
 	else
 	{//else make player run towards it and then execute action later
