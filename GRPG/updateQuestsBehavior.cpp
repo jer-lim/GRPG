08ffa75affad6ea5904f4e0a9b77e7530f23f4ff
Crazoter <crazoter@gmail.com>
2015-02-06 09:54:15 +0800

    questboard should now work, quests finished

--- GRPG/updateQuestsBehavior.cpp
+++ GRPG/updateQuestsBehavior.cpp
@@ -11,32 +11,47 @@ string UpdateQuestsBehavior::displayText(){
 //Finish quests if needed
 void UpdateQuestsBehavior::action()
 {
-	map<int, Quest*>* mapQuests = questLoader->getMapQuests();
-	//loop through quests
-	for (map<int, Quest*>::iterator it = mapQuests->begin(); it != mapQuests->end(); ++it){
-		if (it->second->getQuestCondition()->completed())
-		{//give reward
-			//create gold based on int yeah i'mlazy and out of time sue me
-			InventoryItem* x = new InventoryItem(grpg->getItemLoader()->getItem(0), it->second->getgold());
-			Entity* newObj = new Entity();
-			newObj->initialize(grpg, x, false);
+	VECTOR2 collisionVector;
+	if (player->collidesWith(*entity, collisionVector))
+	{
+		bool updated = false;
+		map<int, Quest*>* mapQuests = questLoader->getMapQuests();
+		//loop through quests
+		for (map<int, Quest*>::iterator it = mapQuests->begin(); it != mapQuests->end(); ++it){
+			if (it->second->getQuestCondition()->completed())
+			{//give reward
+				//create gold based on int yeah i'mlazy and out of time sue me
+				InventoryItem* x = new InventoryItem(grpg->getItemLoader()->getItem(0), it->second->getgold());
+				Entity* newObj = new Entity();
+				newObj->initialize(grpg, x, false);
 
-			int result = player->getInventory()->addEntityInventoryItem(newObj, grpg);
-			if (result == FAILED || result == PARTIAL_MERGE)
-			{
-				//drop the rest on the ground
-				newObj->setX(player->getX());
-				grpg->getDrawManager()->addObject(newObj);
-				newObj->setY(player->getY());
-			}
-			else if (result == MERGED)
-			{
-				//Matt says it should already be handled
-				//delete newObj;
-			}
+				int result = player->getInventory()->addEntityInventoryItem(newObj, grpg);
+				if (result == FAILED || result == PARTIAL_MERGE)
+				{
+					//drop the rest on the ground
+					newObj->setX(player->getX());
+					grpg->getDrawManager()->addObject(newObj);
+					newObj->setY(player->getY());
+				}
+				else if (result == MERGED)
+				{
+					//Matt says it should already be handled
+					//delete newObj;
+				}
 
-			it->second->getQuestCondition()->reset();//reset that quest has been completed
-			ui->addChatText("You have completed a quest!");
+				it->second->getQuestCondition()->reset();//reset that quest has been completed
+				ui->addChatText("You have claimed your rewards for a quest!");
+				updated = true;
+			}
 		}
+		if (!updated)
+		{
+			ui->addChatText("You have no rewards to claim.");
+		}
+	}
+	else
+	{
+		player->setVictim(entity);
+		player->setNPCAction(this);
 	}
 }
