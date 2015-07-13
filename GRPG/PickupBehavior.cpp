6876bb4a47f0b410c00e38179e0b7371537d9e6a
Jeremy Choo <ChooJeremy@Ymail.com>
2015-07-14 01:15:17 +0800

    Added a message if your inventory is full when you attempt to pick an item up

--- GRPG/PickupBehavior.cpp
+++ GRPG/PickupBehavior.cpp
@@ -4,6 +4,7 @@
 //  Student Number:     s10128677
 
 #include "PickupBehavior.h"
+#include "grpg.h"
 #include "DropBehavior.h"
 #include "player.h"
 
@@ -11,9 +12,13 @@ void PickupBehavior::action(){
 	VECTOR2 collisionVector;
 	if (entity->collidesWith(*player, collisionVector))
 	{//if it is range with player
-		player->getInventory()->addEntityInventoryItem(entity, (Grpg*)gamePtr);//add entity into inventory
+		INVENTORY_CHANGE result = player->getInventory()->addEntityInventoryItem(entity, (Grpg*)gamePtr);//add entity into inventory
 		//delete this;//THIS MUST BE CALLED IF YOU INTEND TO REMOVE THE BEHAVIOR
 		//deleted in destructor to account for merge condition
+		if (result == INVENTORY_CHANGE::FAILED || result == INVENTORY_CHANGE::PARTIAL_MERGE)
+		{
+			((Grpg*)gamePtr)->getUI()->addChatText("Your inventory is full. Try dropping something to make space.");
+		}
 	}
 	else
 	{//else make player run towards it and then execute action later
